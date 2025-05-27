c759c1cd62294189e1de267b7cea0a45f3918bc0
Ilysse200 <ilysseted500@gmail.com>
2025-05-27 23:32:29 +0200

    Modified blog routes to add authorization to create, update and delete a blog for only admin

--- src/routes/blogRoutes.ts
+++ src/routes/blogRoutes.ts
@@ -1,12 +1,13 @@
 import * as express from 'express'
 import { blogController } from '../controller/Blog.controller';
 import { authentification } from '../middleware/auth.middleware';
+import { authorizeRole } from '../middleware/authorization.middleware';
 const Blogrouter = express.Router();
 
 
-Blogrouter.post('/createBlogs', authentification as any, blogController.createPost as any);
+Blogrouter.post('/createBlogs', authentification as any,authorizeRole("admin") as any, blogController.createPost as any);
 Blogrouter.get('/getBlogs', authentification as any, blogController.getAllPosts as any)
-Blogrouter.put('/editBlogs/:id', authentification as any, blogController.updatePost as any );
+Blogrouter.put('/editBlogs/:id', authentification as any,authorizeRole("admin") as any, blogController.updatePost as any );
 Blogrouter.get('/getBlogsById/:id', authentification as any, blogController.getPostById as any);
-Blogrouter.delete('/deleteBlog/:id', authentification as any, blogController.deleteBlog as any)
+Blogrouter.delete('/deleteBlog/:id', authentification as any,authorizeRole("admin") as any, blogController.deleteBlog as any)
 export default Blogrouter;
