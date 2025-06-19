29416edac53dea25ca49e56e3ef7a22801f0d05f
Ilysse200 <ilysseted500@gmail.com>
2025-06-19 08:09:11 +0200

    modified routes and removed authorization and authentication check

--- src/routes/blogRoutes.ts
+++ src/routes/blogRoutes.ts
@@ -2,13 +2,13 @@ import * as express from 'express'
 import {createPost, deleteBlogController, getAllPosts, blogController} from "../controller/Blog.controller"
 import { authentification } from '../middleware/auth.middleware';
 import { authorizeRole } from '../middleware/authorization.middleware';
-import { validate } from '../middleware/validate.middleware';
-import { createBlogSchema } from '../schema/blog.schema';
+// import { validate } from '../middleware/validate.middleware';
+// import { createBlogSchema } from '../schema/blog.schema';
 const Blogrouter = express.Router();
 
 
-Blogrouter.post('/createBlogs', authentification as any,validate(createBlogSchema), createPost);
-Blogrouter.get('/getBlogs', authentification as any, getAllPosts as any)
+Blogrouter.post('/createBlogs',createPost);
+Blogrouter.get('/getBlogs', getAllPosts as any)
 // Blogrouter.put('/editBlogs/:id', authentification as any, blogController.updatePost as any );
 Blogrouter.get('/getBlogsById/:id', authentification as any, blogController.getPostById as any);
 Blogrouter.delete('/deleteBlog/:id', authentification as any,authorizeRole("admin") as any, deleteBlogController)
